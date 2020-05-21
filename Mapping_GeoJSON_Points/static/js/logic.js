7bb99aff21039377cc930e32f32bed4fbbbe56c9
Helen-Ly <helenly25@gmail.com>
2020-05-20 23:27:04 -0400

    Add multiple-point type GeoJSON data to the map.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,36 +2,7 @@
 console.log('working');
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data
-L.geoJSON(sanFranAirport, {
-
-	onEachFeature: function(feature, layer) {
-		console.log(layer);
-		layer.bindPopup('<h4> Airport code: ' + layer.feature.properties.faa + '<hr> Airport name: ' + layer.feature.properties.name + '</h3>');
-	}
-}).addTo(map);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -41,3 +12,18 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+//https://raw.githubusercontent.com/Helen-Ly/Mapping_Earthquakes/master/majorAirports.json
+// Have the tilelayer() before ensures that the map gets loaded before the data is added to it
+let airportData = 'https://raw.githubusercontent.com/Helen-Ly/Mapping_Earthquakes/master/majorAirports.json';
+
+// Grabbing our GeoJSON data
+d3.json(airportData).then(function(data) {
+	console.log(data);
+
+	// Creating a GeoJSON layer with the retrieved data
+	L.geoJson(data)
+	.bindPopup()
+	.addTo(map);
+});
